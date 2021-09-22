62512943691107585d686d7b8cd18970f55da9b1
Jeremy Jensen <jcjensen62@gmail.com>
2021-09-21 20:23:16 -0600

    JavaScript changes

--- public/main.js
+++ public/main.js
@@ -1,15 +1,40 @@
-let arrList = ['Homework', 'Dishes', 'Walk the dog']
+let arrList = [{
+  "title": "Eat",
+  "category": "Hungry",
+  "id": 1
+},
+{
+  "title": "Sleep",
+  "category": "Tierd",
+  "id": 2
+},
+{
+  "title": "Work",
+  "category": "Living",
+  "id": 3
+}]
 
-function CreateItems(arr) {
-    for(let i = 0; i < arr.length; ++i){
-      let list = document.getElementById('toDoList')
+function DisplayItems(arr) {
+    arr.forEach(element => {
+      let list = document.querySelector('#toDoList')
       let item = document.createElement('li')
 
-      item.appendChild(document.createTextNode(arr[i]))
+      item.appendChild(document.createTextNode(element.title + ' Category: ' + element.category))
       list.appendChild(item)
-    }
 
-    return list
+      //Adds needed class for bootstrap
+      item.setAttribute()
+    });
+  
+  
+  
+  // for(let i = 0; i < arr.length; ++i){
+    //   let list = document.getElementById('toDoList')
+    //   let item = document.createElement('li')
+
+    //   item.appendChild(document.createTextNode(arr[i]))
+    //   list.appendChild(item)
+    // }
 }
 
-CreateItems(arrList)
+DisplayItems(arrList)
