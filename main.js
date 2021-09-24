30eac36d897abbf51a5df2857b2128aead3c1e86
Jeremy Jensen <jcjensen62@gmail.com>
2021-09-23 21:33:30 -0600

    Mark as complete

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 let todoList = [{
   title: "Eat",
-  id: 1
+  id: 1,
+  complete: "false"
 },
 {
   title: "Sleep",
-  id: 2
+  id: 2,
+  complete: "false"
 }, 
 {
   title: "Work",
-  id: 3
+  id: 3,
+  complete: "false"
 }]
 
+// Add Item to do
 const userObject = {}
 const addItemBtn = document.querySelector('#addItem')
 
 addItemBtn.addEventListener('click', event => {
-  console.log("yay clicked")
   let newTask = {
     title: document.querySelector('#userTask').value,
     id: todoList.length + 1
@@ -24,6 +27,25 @@ addItemBtn.addEventListener('click', event => {
   todoList.push(newTask)
   DisplayItems(todoList)
   document.querySelector('#userTask').value = ""
+})
+
+//Mark as complete
+document.addEventListener('click', event => {
+  let element = event.target;
+    if(element.tagName == 'INPUT' && element.classList.contains("form-check-input")){
+      for (const item of todoList) {
+        if (parseInt(event.target.id) === item.id) {
+          switch(event.target.value){
+            case 'on':
+              item.complete = true
+              break
+            case 'off':
+              item.complete = false
+              break
+          }
+        }
+      }
+    }
 })
 
 function DisplayItems(arr) {
@@ -37,6 +59,7 @@ function DisplayItems(arr) {
       const itemCheckmark = document.createElement('input')
       itemCheckmark.classList.add('form-check-input')
       itemCheckmark.type = 'checkbox'
+      itemCheckmark.setAttribute('id', `${element.id}`)
       itemDiv.appendChild(itemCheckmark)
 
       // Creates li
