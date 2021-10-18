24021358c4f81b323284c2be581af9d199483656
BrNel <35586279+BriNelson@users.noreply.github.com>
2021-10-18 02:41:49 -0600

    nodes servers sorta works now

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import todoData from "./todoList.js";
 const app = Express();
 const port = 3000;
 
-app.get("/todoData", (req, res) => {
+app.get("/", (req, res) => {
     res.json(todoData)
     
   })
