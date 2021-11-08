6a129fc70c48329552bfb2d8193caf12d67544a3
BrNel <35586279+BriNelson@users.noreply.github.com>
2021-11-08 15:50:35 -0700

    post side basically done

--- postTodos.js
+++ postTodos.js
@@ -8,18 +8,22 @@ app.post("/todo", (req, res) => {
     const todo = req.body
     const todo = new todoModel({
        
+        title: todo.title,
+       
+        complete: false,
+        category: 'Work',
 
-        
-   })
+    })
+    todo.save().then((result) => {console.log(result)});
    
  
-   todoData.push({
-     title: todo.title,
-     id: todoData.length + 1,
-     complete: false,
-     category: 'Work',
-     })
-   console.log(todoData)
+//    todoData.push({
+//      title: todo.title,
+//      id: todoData.length + 1,
+//      complete: false,
+//      category: 'Work',
+//      })
+//    console.log(todoData)
    // res.send('todo is added to the database');
  
   })
