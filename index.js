21dc778a00e6e94c356c9206d83d943f222cd825
BrNel <35586279+BriNelson@users.noreply.github.com>
2021-11-08 12:57:30 -0700

    mongo db branch started

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
 
 import express from 'express';
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 // const express = require('express')
 // const bodyParser = require('body-parser');
-let todoData = [
-  {
-    "title": "Eat",
-    "id": 1,
-    "complete": false,
-    "category": "Personal"
-  },
-  {
-    "title": "Sleep",
-    "id": 2,
-    "complete": false,
-    "category": "Personal"
-  },
-  {
-    "title": "Code",
-    "id": 3,
-    "complete": false,
-    "category": "Work"
-  }
-];
+
 const app = express();
 const port = 3000;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-
-app.use(express.static('C:/Users/ladof/Desktop/m1-todo-app-501st'));
-
-app.get("/todoData", (req, res) => {
-  res.json(todoData)
-    // res.send(req.params)
-  
+mongoose.connect('mongodb+srv://3727137271:3727137271@cluster0.olv4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useMongClient = true
 })
+  .then(() => {
+  console.log('Connected Successfully')
+})
+.catch(console.error)
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+
+// // app.use(express.static('C:/Users/ladof/Desktop/m1-todo-app-501st'));
+
+// app.get("/todoData", (req, res) => {
+//   res.json(todoData)
+//     // res.send(req.params)
+  
+// })
   
 
-app.post("/todo", (req, res) => {
-  const todo = req.body
+// app.post("/todo", (req, res) => {
+//   const todo = req.body
   
-  // console.log(JSON.parse(todo));
+//   // console.log(JSON.parse(todo));
 
-  todoData.push({
-    title: todo.title,
-    id: todoData.length + 1,
-    complete: false,
-    category: 'Work',
-    
-    
-  
-  })
-  console.log(todoData)
-  // res.send('todo is added to the database');
+//   todoData.push({
+//     title: todo.title,
+//     id: todoData.length + 1,
+//     complete: false,
+//     category: 'Work',
+//     })
+//   console.log(todoData)
+//   // res.send('todo is added to the database');
 
- })
+//  })
 
 
 
 
-app.listen(port, () => console.log(port))
+// app.listen(port, () => console.log(port))
